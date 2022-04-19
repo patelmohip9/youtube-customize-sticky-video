a85c260bdaa97896e7a3031452e22e042138404a
mohip patel <patelmohip9@gmail.com>
2022-04-19 11:39:43 +0530

    jquery condition

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '29080978210fa0c2ca02c77bf256bbdb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e08633cd0b6f5710cf5b40a56614488c');
